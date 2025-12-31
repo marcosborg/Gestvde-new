5889f99b499a15c21401eaec27725b78daae49fe
Marcos Borges <110690829+marcosborg@users.noreply.github.com>
2025-12-31 09:19:24 +0000

    Update dashboard

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -15,8 +15,6 @@ use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Widgets\AccountWidget;
-use Filament\Widgets\FilamentInfoWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -48,8 +46,6 @@ class AdminPanelProvider extends PanelProvider
                 ExpenseByCategoryChart::class,
                 UpcomingEventsStatsWidget::class,
                 PendingTasksWidget::class,
-                AccountWidget::class,
-                FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
